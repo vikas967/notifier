7f38b4d9bcf7960ce0822f9310dbdc3efedc306e
vikas967 <mathurvikas000@gmail.com>
2025-04-20 11:56:07 +0530

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        
     },
 });
 
